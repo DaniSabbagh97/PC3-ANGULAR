dde81a4c8c5955804ea24856674472b1a6316973
DaniSabbagh97 <44496284+DaniSabbagh97@users.noreply.github.com>
2021-06-13 17:43:16 +0200

    Controlar que no haya imagenes en el alquiler

--- pc3-laravel/app/Http/Python/EscapadaRuralBusqueda.py
+++ pc3-laravel/app/Http/Python/EscapadaRuralBusqueda.py
@@ -2,14 +2,33 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import mysql.connector
+import sys
+
+def normalize(s):
+    replacements = (
+        ("á", "a"),
+        ("é", "e"),
+        ("í", "i"),
+        ("ó", "o"),
+        ("ú", "u"),
+    )
+    for a, b in replacements:
+        s = s.replace(a, b)
+    return s
 
 myconn = mysql.connector.connect(host="127.0.0.1", user="root", passwd="", database="silentlife")
 cur = myconn.cursor()
 
 
 sql = "insert into alquileres (nombre, descripcion, fotos, ubicacion, coste, especificaciones) values (%s, %s, %s, %s, %s, %s)"
-ciudad = "cataluña"
-page = requests.get('https://www.escapadarural.com/casas-rurales?l=',ciudad)
+ciudad = sys.argv[1]
+
+
+ciudad2 = ciudad.replace(" ","-")
+ciudad3 = normalize(ciudad2.lower())
+print(ciudad3)
+page = requests.get('https://www.escapadarural.com/casas-rurales?l='+ciudad3)
+
 soup = BeautifulSoup (page.content, 'html.parser')
 #print(soup.prettify())
 html = list(soup.children)[4]
@@ -21,38 +40,56 @@ for i in article:
     h3 = list(i.findAll("h3"))
     h4 = list(i.findAll("h4"))
     descripcion = list(i.findAll("div", class_="c-result--abstract"))
-    totalImg = i.find('img')['data-src']
+    try:
+        totalImg = i.find('img')['data-src']
+    except:
+        totalImg = "Null"
     precio = (i.findAll('div', class_="c-result--price"))
     especificaciones = (i.findAll('div', class_="c-result--item"))
-    print(totalImg) #TODO GUARDAR IMAGEN
+    print(totalImg)#TODO GUARDAR IMG
+
     for a in h3:
-        quitar = a.text
-        sinEspacio = re.sub(r'(\s\s*)', ' ',quitar)
-        print("Vivienda:",sinEspacio)#TODO GUARDAR H3
+        if a == None:
+            sinEspacio='Null'
+        else:
+            quitar = a.text
+            sinEspacio = re.sub(r'(\s\s*)', ' ',quitar)
+            print("Vivienda:",sinEspacio)#TODO GUARDAR H3
 
     for b in h4:
-        lugar = b.text
-        print("Lugar: "+lugar)#TODO GUARDAR LUGAR
+        if b == None:
+            lugar = 'Null'
+        else:
+            lugar = b.text
+            print("Lugar: "+lugar)#TODO GUARDAR LUGAR
     for c in descripcion:
-        quitar2 = c.text
-        sinEspacio2 = re.sub(r'(\s\s*)', ' ', quitar2)
-        print("Descripcion:",sinEspacio2)#TODO GUARDAR H4
+        if c == None:
+            sinEspacio2 = 'Null'
+        else:
+            quitar2 = c.text
+            sinEspacio2 = re.sub(r'(\s\s*)', ' ', quitar2)
+            print("Descripcion:",sinEspacio2)#TODO GUARDAR H4
     for d in precio:
-        precioSinRegEx = d.text
-        precioConRegEx = re.sub(r'(\s\s*)',' ', precioSinRegEx)
-        print("Precio: "+precioConRegEx)#TODO GUARDAR PRECIO
-    print("Especificaciones:")
+        if d == None:
+            precioConRegEx = 'Null'
+        else:
+            precioSinRegEx = d.text
+            precioConRegEx = re.sub(r'(\s\s*)',' ', precioSinRegEx)
+            print("Precio: "+precioConRegEx)#TODO GUARDAR PRECIO
+            print("Especificaciones:")
     for e in especificaciones:
-        datosSinRegEx = e.text
-        datosConRegEx = re.sub(r'(\s\s*)', ' ', datosSinRegEx)
-        print(datosConRegEx+", ")#TODO GUARDAR ESPEXIFICACIONES
-    # listaH3 = []
-    # listaH3.append(sinEspacio)
+        if e == None:
+            datosConRegEx='Null'
+        else:
+            datosSinRegEx = e.text
+            datosConRegEx = re.sub(r'(\s\s*)', ' ', datosSinRegEx)
+            print(datosConRegEx+", ")#TODO GUARDAR ESPEXIFICACIONES
+
     val = (sinEspacio, sinEspacio2, totalImg, lugar, precioConRegEx, datosConRegEx)#TODO SOLO EN UN INSERT
     try:
-        # inserting the values into the table
+        # ejecutando la query que le hemos dicho a val
         cur.execute(sql, val)
-        # commit the transaction
+        # commit de la conexion
         myconn.commit()
 
     except:
